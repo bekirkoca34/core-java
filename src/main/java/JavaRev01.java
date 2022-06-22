ca75841c743bdf805e7af33b1799ffc9722f8c40
bekirkoca34 <bekirkoca8484@gmail.com>
2022-06-22 06:16:38 +0100

    new java code is created

---
+++ src/main/java/JavaRev01.java
@@ -0,0 +1,7 @@
+public class JavaRev01 {
+    public static void main(String[] args) {
+        System.out.println("my java project is working well");
+
+        System.out.println(" today, I am planning to revise to array list in Java");
+    }
+}
