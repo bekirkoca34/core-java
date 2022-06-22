968cc43cebbe287d7cd444c9d2670fb59a464855
bekirkoca34 <bekirkoca8484@gmail.com>
2022-06-22 06:24:09 +0100

    new updated is saved

--- src/main/java/JavaRev01.java
+++ src/main/java/JavaRev01.java
@@ -3,5 +3,7 @@ public class JavaRev01 {
         System.out.println("my java project is working well");
 
         System.out.println(" today, I am planning to revise to array list in Java");
+        System.out.println("second version was updated");
+
     }
 }
